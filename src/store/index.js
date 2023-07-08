d64b6e1c83d336394468f36ae3fdebdb07892006
Adil <89954695+programest@users.noreply.github.com>
2023-07-08 19:37:55 +0600

    Delete src directory

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore, combineReducers, compose } from 'redux'
-import { reducerLogicInput } from '../reducer/reducerLogic'
-import { composeWithDevTools } from 'redux-devtools-extension'
-const rootReducer = combineReducers({
-	logic: reducerLogicInput,
-})
-export const store = createStore(rootReducer, composeWithDevTools())
